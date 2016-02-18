bdd77f84f489ccbf73575fa5ce76107a88cc88e4
Mike Morrow <mmorrow@vt.edu>
2016-02-18 11:47:31 -0500

    Remove check_mk and php check.

--- centos-7.2-nagios/scripts/nagios.sh
+++ centos-7.2-nagios/scripts/nagios.sh
@@ -13,13 +13,10 @@ echo -e "\n\nsecret\nsecret\n\n\nn\n\n " | mysql_secure_installation 2>/dev/null
 # Install php
 # ---------------------------------------------------------------------------
 yum install php php-mysql php-gd php-pear -y
-cp /vagrant/conf/info.php /var/www/html/info.php
-systemctl restart httpd
 # ---------------------------------------------------------------------------
 # Install nagios
 # ---------------------------------------------------------------------------
 yum install nagios -y
-yum install --nogpgcheck -y https://mathias-kettner.de/support/1.2.6p16/check-mk-raw-1.2.6p16-el7-34.x86_64.rpm
 # ---------------------------------------------------------------------------
 # Install base plugins
 # ---------------------------------------------------------------------------
